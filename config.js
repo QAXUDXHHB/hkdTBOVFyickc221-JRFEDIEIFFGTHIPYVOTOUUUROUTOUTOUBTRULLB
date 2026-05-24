9d162c16af74e7c7cd40cc86b5a75558a7bb9def
QAXUDXHHB <bucbdfshidf@outlook.com>
2026-05-24 13:38:40 +0800

    创建 config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const MUSIC_CFG = {
+    enable: true,
+    url: "https://assets.mixkit.co/sfx/download/mixkit-alert-system-995.mp3",
+    volume: 1.0,
+    loop: true,
+    backups: [
+        "https://assets.mixkit.co/sfx/preview/mixkit-alarm-tone-996.mp3",
+        "https://assets.mixkit.co/sfx/download/mixkit-digital-alarm-1119.mp3"
+    ]
+};
+）
+const TEXT_CFG = {
+    content: "程序版本1.0.2.8",
+    count: 1200,
+    fontSize: 10,
+    speedY: [60, 120],
+    speedX: [-15, -5],
+    spawnY: [-800, 0]
+};
